21e98ee1f1baaf3287826a1d3d9398b8934d2935
haido242 <khuyenn239@gmail.com>
2022-12-01 15:20:09 +0700

    fist commit

---
+++ models/Group.js
@@ -0,0 +1,11 @@
+
+class Group{
+    constructor(GroupName, GroupId){
+        this.type = "Group";
+        this.GroupName = GroupName;
+        this.GroupId = GroupId;
+    }
+
+}
+
+module.exports = Group
