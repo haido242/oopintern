777f27d91d2133a823d0d0f1ec991bc7e835c3fb
haido242 <khuyenn239@gmail.com>
2022-12-12 17:20:42 +0700

    create a bug

--- Controller/userController.js
+++ Controller/userController.js
@@ -1,54 +1,49 @@
 const User = require("../models/User");
-const { addMemberGroup } = require("./GroupController");
-const baseController = require("./BaseController");
-const collection = "user";
-class userController extends User{
-    constructor(){
-        super(User)
-    }
-//     createUser(req, res) {
-//     try {
-//       const newUser = new User(req.body);
-//       baseController.create(newUser, "user").then((data) => {
-//         addMemberGroup(req.body.GroupId, data.insertedId);
-//       });
+const userModel = new User()
+class userController {
 
-//       res.json("add user success");
-//     } catch (err) {
-//       console.log(err);
-//       res.json("add user fail");
-//     }
-//   }
+  async createUser(req, res) {
+    try {
+      const newUser = req.body;
+      await userModel.add(newUser)
+
+      res.json("add user success");
+    } catch (err) {
+      console.log(err);
+      res.json("add user fail");
+    }
+  }
   async getUserList(req, res) {
     try {
-      const data = await super.getUser()
-      data.toArray().then((data)=> {res.json(data)})
+      const data = await userModel.get()
+      data.toArray().then((data) => res.json(data))
     } catch (err) {
       console.log(err);
       res.json("get list user is fail!");
     }
   }
-//   deleteUser(req, res) {
-//     try {
-//       const id = req.params["id"];
-//       baseController.deleteData(id, collection);
-//       res.status(200).json("delete success");
-//     } catch (err) {
-//       console.log(err);
-//       res.status(500).json("delete fail!");
-//     }
-//   }
-//   updateUser(req, res) {
-//     try {
-//       const id = req.params["id"];
-//       const data = req.body;
-//       baseController.update(id, data, collection);
-//       res.status(200).json("updated");
-//     } catch (err) {
-//       console.log(err);
-//       res.json("update fail!");
-//     }
-//   }
+  async deleteUser(req, res) {
+    try {
+      const id = req.params["id"];
+      await userModel.del(id)
+      res.status(200).json("delete success");
+    } catch (err) {
+      console.log(err);
+      res.status(500).json("delete fail!");
+    }
+  }
+  async updateUser(req, res) {
+    try {
+      const id = req.params["id"];
+      const data = req.body;
+      await userModel.update(id, data)
+      res.status(200).json("updated");
+    } catch (err) {
+      console.log(err);
+      res.json("update fail!");
+    }
+  }
+  async getUserByid(req, res){}
 }
 
 module.exports = userController;
